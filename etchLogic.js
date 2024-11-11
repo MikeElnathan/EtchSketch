ce860999f0204915bf60247cfeb88074dd0575df
MikeElnathan <mikeson.anthony@gmail.com>
2024-11-11 14:38:26 +0800

    Completed the minimum requirement of the task. : 1. Create a grid based on number input by user. 2. Change the color of the grid as mouse hover over it.

--- etchLogic.js
+++ etchLogic.js
@@ -3,24 +3,58 @@ const startBtn = document.getElementById("start");
 const cont = document.getElementById("container");
 // -------------
 
-startBtn.addEventListener("click", startEtch, {once : true});
+startBtn.addEventListener("click", startEtch);
 
 function startEtch()
 {
-    startBtn.remove();
-    // add reset button here to start over.
-    cont.style.cssText = "width: 160px; height: 160px";
-
-    for (i = 0; i < 256; i++)
+    if (cont.childElementCount > 0)
+        {
+            cont.replaceChildren("");
+            createGrid()
+        }
+    else
     {
-        const divE = document.createElement("div");
-        divE.style.cssText = 
-        " background-color: white; width: 10px; height: 10px; outline: 1px solid black;";
-        console.log("div created");
-        cont.appendChild(divE);
+        createGrid();
+    }  
+}
+
+function createGrid()
+{
+    let size = sketchSize();
+
+    while (isNaN(size) || (size <= 0) || (size > 100))
+    {
+            size = sketchSize();
     }
 
-    testCall();
+    for (let i = 0; i < size*size; i++)
+    {
+            const divE = document.createElement("div");
+            
+            divE.style.flex = "1 1 auto";
+            divE.style.outline = "1px solid black";
+            divE.style.aspectRatio = "1/1";
+            divE.style.backgroundColor = "white";
+            divE.style.width = `${500/size}px`;
+            divE.style.height = `${500/size}px`;
+
+            let a = 500/size;
+            console.log(a);
+
+            //add event listener for mouse hover
+            divE.addEventListener("mouseover", () =>{
+                divE.style.backgroundColor = "black";
+            });
+            cont.appendChild(divE);
+    }
+
+}
+
+function sketchSize()
+{
+    let size = parseInt(prompt("Enter number from 1 to 100"));
+
+    return size;
 }
 
 function testCall()
